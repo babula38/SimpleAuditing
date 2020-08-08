66a35a135bece4dc0bdd203350c1e6cacd24f47e
Babu <babula38@gmail.com>
2020-08-08 22:52:28 +0530

    Add project files.

---
+++ SimpleAuditing/AuditingAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SimpleAuditing
+{
+    public class AuditingAttribute : ActionFilterAttribute
+    {
+        private readonly string field;
+
+        public AuditingAttribute(string field = "*")
+        {
+            this.field = field;
+        }
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+            var arg = context.ActionArguments["name"] as dynamic;
+                         
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            
+            base.OnActionExecuted(context);
+        }
+    }
+}
---
+++ SimpleAuditingApi/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleAuditing;
+using SimpleAuditingApi;
+
+namespace SimpleAuditingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() => "Hello";
+
+        [Auditing(field: "Name.ID")]
+        [HttpPost]
+        public void Post(MyClass name) { }
+    }
+
+    public class MyClass
+    {
+        public int ID { get; set; }
+        public Inner MyProperty { get; set; }
+    }
+    public class Inner
+    {
+        public string MyInner { get; set; }
+    }
+}
